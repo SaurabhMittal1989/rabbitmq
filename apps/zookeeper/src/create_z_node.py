f66647b2d0a9b7594808ba3665c0be25c061167a
Saurabh Mittal <saurabh.mech.ism@gmail.com>
2024-09-09 17:32:10 +0530

    updating zookeeper client code

--- apps/zookeeper/src/create_z_node.py
+++ apps/zookeeper/src/create_z_node.py
@@ -1,40 +1,53 @@
 import time
-import traceback
+from kazoo.recipe.watchers import ChildrenWatch
 
-from apps.zookeeper.src.zk_client import zk, create_zk_client
-
-
-def watcher(event):
-    # check to see what the children are now
-    print("event: ", event)
-    children = zk.get_children("/cloudxlab/", watch=watcher)
-    print(children)
-
-
-# create a znode in Zookeeper
-def create_node(path, *args, **kwargs) -> bool:
-    """Returns True if Success"""
-    try:
-        zk.create("path", args, kwargs)
-        return True
-    except Exception as e:
-        print(f"failed to craete Zoo Keeper Node: {e}")
-        traceback.print_exc()
-        return False
-
+from apps.zookeeper.src.zk_client import ZooKeeper
 
 if __name__ == "__main__":
-
     # Call my_func when the children change
-    create_zk_client()
-    children = zk.get_children("/cloudxlab/", watch=watcher)
-    print(children)
 
-    children = zk.create("/cloudxlab/a", ephemeral=True) if not zk.exists("/cloudxlab/a") else None
-    children = zk.create("/cloudxlab/b", ephemeral=True) if not zk.exists("/cloudxlab/b") else None
+    zk = ZooKeeper()
+    zk.delete_node("/cloudxlab/", recursive=True)
 
-    zk.stop()
+    zk.print_node_tree("", "START")
+
+    zk.create_node("/cloudxlab")
+    ChildrenWatch(client=zk.zk,path="/cloudxlab", func=zk.watcher)
+    zk.create_node("/cloudxlab/a")
+    time.sleep(1)
+    zk.create_node("/cloudxlab/b")
+
+    zk.create_node("/cloudxlab/b/1", ephemeral=True)
+    zk.create_node("/cloudxlab/b/2", ephemeral=True)
+    zk.create_node("/cloudxlab/a/1", ephemeral=True)
+    zk.create_node("/cloudxlab/a/2", ephemeral=True)
+
+    zk.create_node("/cloudxlab/c", sequence = True, value=b"1",ephemeral=True)
+    zk.create_node("/cloudxlab/c", sequence=True, value=b"2",ephemeral=True)
+    zk.create_node("/cloudxlab/c", sequence=True, value=b"3",ephemeral=True)
+    zk.create_node("/cloudxlab/c", sequence=True, value=b"4",ephemeral=True)
+
+    # Start a transaction
+    transaction = zk.zk.transaction()
+
+    # Add multiple create operations to the transaction
+    transaction.create('/node1', b'data1')
+    transaction.create('/node2', b'data2')
+    transaction.create('/node3', b'data3')
+
+    # Commit the transaction
+    results = transaction.commit()
+
+    # Check the results
+    for result in results:
+        print(result)
+
+
+    zk.print_node_tree("", "END")
     time.sleep(5)
-    zk.start()
-
+    zk.zk.stop()
+    time.sleep(5)
+    zk.zk.start()
+    time.sleep(5)
+    zk.print_node_tree("", "AFTER RECONNECT")
     time.sleep(100)
--- apps/zookeeper/src/zk_client.py
+++ apps/zookeeper/src/zk_client.py
@@ -1,22 +1,65 @@
 from kazoo.client import KazooClient
+from kazoo.exceptions import NodeExistsError, ZookeeperError
+from kazoo.protocol.states import KazooState
 
-zk = None
-
-
-def create_zk_client():
-    global zk
-    if not zk:
-        zk = KazooClient(hosts='127.0.0.1:2181')
-        zk.start()
-        zk.add_listener(connection_listener)
-    else:
-        return zk
+def connection_listener(state: KazooState):
+        print(f"Client is {state} from ZooKeeper")
 
 
-def connection_listener(state):
-    if state == "DISCONNECTED":
-        print("Client is disconnected from ZooKeeper")
-    elif state == "CONNECTED":
-        print("Client is connected to ZooKeeper")
-    elif state == "LOST":
-        print("Client lost connection to ZooKeeper")
+class ZooKeeper:
+    zk =None
+    @staticmethod
+    def __init__():
+        if not ZooKeeper.zk:
+            ZooKeeper.zk = KazooClient(hosts='127.0.0.1:2181')
+            ZooKeeper.zk.start()
+            ZooKeeper.zk.add_listener(connection_listener)
+
+    def watcher(self, event):
+        # check to see what the children are now
+        self.print_node_tree(path="", event=event)
+
+    def create_node(self, path, *args, **kwargs) -> str:
+        """Create a Znode in Zookeeper
+        Returns path if Success"""
+        try:
+            path = self.zk.create(path, *args, **kwargs)
+            return path
+        except NodeExistsError as e:
+            print(f"failed to create Zoo Keeper Node: {path}. Exception {e.__repr__()}")
+            # traceback.print_exc()
+
+    def delete_node(self, path, *args, **kwargs) -> bool:
+        """Delete a znode in Zookeeper
+        Returns True if Success"""
+        try:
+            self.zk.delete(path, *args, **kwargs)
+            return True
+        except ZookeeperError as e:
+            print(f"failed to delete ZNode: {e}")
+            # traceback.print_exc()
+            return False
+
+    def print_node_tree(self, path="", event=""):
+        """Thread Safe Print"""
+        tree = []
+        self._print_node_tree(path, tree=tree)
+        newline = "\n"
+        print(f"----------{event}----------------\n"
+              f"{newline.join(tree)}{newline}"
+              f"--------------------------")
+
+    def _print_node_tree(self, path, tree):
+        if self.zk.exists(path):
+            data, stat = self.zk.get(path)
+            line = f"ZNode: {path}, Data: {data.decode('utf-8')}, Version: {stat.version}"
+            tree.append(line)
+        else:
+            print(f"Znode does not exist: {path}")
+        for child in self.zk.get_children(path):
+            self._print_node_tree(f"{path}/{child}", tree=tree)
+
+
+
+
+
